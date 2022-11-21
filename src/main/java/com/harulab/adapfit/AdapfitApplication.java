a0d957cb14e9e54b813fba506b14ec4dc7696555
wonyongChoi05 <rltgjqmduftlagl@gmail.com>
2022-11-22 08:21:07 +0900

    feat(jwt): jwt 프로퍼티 바인딩을 위한 JwtProperties를 생성했다.

--- src/main/java/com/harulab/adapfit/AdapfitApplication.java
+++ src/main/java/com/harulab/adapfit/AdapfitApplication.java
@@ -2,8 +2,10 @@ package com.harulab.adapfit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class AdapfitApplication {
 
 	public static void main(String[] args) {
